52239eb1ce705cf78806f5791506121159caaba3
andreisineiko-eng <andrei.sineiko@gmail.com>
2026-03-02 12:12:23 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <circle cx="16" cy="16" r="16" fill="#111"/>
+  <rect x="9" y="9" width="14" height="14" rx="4" fill="url(#g)"/>
+  <defs>
+    <linearGradient id="g" x1="0" y1="0" x2="1" y2="1">
+      <stop offset="0%" stop-color="#FF6B35"/>
+      <stop offset="100%" stop-color="#C850C0"/>
+    </linearGradient>
+  </defs>
+</svg>
